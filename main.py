82732490636d2e946db7a87ac2484d9cb10cfc83
Farzeen <happycoder97@protonmail.com>
2021-02-08 22:55:26 +0530

    clean up main.py, specify folder in cli args

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
+#!/bin/python
 import urwid
 import os
-
-from bread_crumb import BreadCrumb
-from button import Button
+import sys
 from explorer_widget import ExplorerWidget
 from explorer import Explorer
 from pathlib import Path
 
+if len(sys.argv) != 2: # argv[0] is main.py
+    print("Usage: dml <directory>")
+    sys.exit(-1)
 
-def exit_on_q(key):
+dml_dir = Path(sys.argv[1])
+
+if dml_dir.is_file():
+    print("Given directory is actually a file.")
+    sys.exit(-1)
+
+if not dml_dir.exists():
+    print("Directory does not exist.")
+    sys.exit(-1)
+
+
+def unhandled_input(key):
     if key in ["esc"]:
         raise urwid.ExitMainLoop()
 
@@ -21,7 +34,14 @@ palette = [
     ("default", "default", "default"),
 ]
 
-root = urwid.Padding(ExplorerWidget(Explorer(Path("."))), align="center", width=120)
-loop = urwid.MainLoop(root, palette, unhandled_input=exit_on_q, pop_ups=True)
+
+root = urwid.Padding(
+    ExplorerWidget(
+        Explorer(Path(dml_dir)),
+    ),
+    align="center",
+    width=120,
+)
+
+loop = urwid.MainLoop(root, palette, unhandled_input=unhandled_input, pop_ups=True)
 loop.run()
-# dml.save("test.json")
