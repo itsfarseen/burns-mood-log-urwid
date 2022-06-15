b5d7887bf4e73cc7d021580e9b26e9128781821a
Farseen <oss@itsfarseen.anonaddy.me>
2022-06-15 07:15:38 +0530

    Provide nix flakes

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "A basic flake with a shell";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in rec {
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = [ pkgs.bashInteractive ];
+        buildInputs = [ 
+          pkgs.python310 
+          pkgs.python310Packages.urwid 
+          pkgs.python310Packages.cryptography
+        ];
+      };
+      packages.dml = pkgs.stdenv.mkDerivation rec {
+        name = "dml-${version}";
+        version = "0.0.4";
+        src = builtins.path { name = "dml"; path = ./.; };
+        buildInputs = [
+          (pkgs.python310.withPackages (
+            pyPkgs: [
+              pyPkgs.urwid
+              pyPkgs.cryptography
+            ]
+          ))
+        ];
+        installPhase = ''
+          mkdir -p $out/dml;
+          cp -r . $out/dml
+          mkdir -p $out/bin;
+          dd status=none of=$out/bin/dml << EOF
+          #!/bin/bash
+          ${pkgs.python310}/bin/python $out/dml/main.py \$*
+          EOF
+          chmod +x $out/bin/dml
+        '';
+      };
+      packages.default = packages.dml;
+    });
+}
